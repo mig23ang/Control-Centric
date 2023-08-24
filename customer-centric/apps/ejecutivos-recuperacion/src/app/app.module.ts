08324049d9fcc37d1436a8e2d814a07daa7bd9a5
Miguel Angel Reyes <trc_miguel.reyes@bancompartir.co>
2023-08-24 08:19:24 -0500

    cambios ok

--- customer-centric/apps/ejecutivos-recuperacion/src/app/app.module.ts
+++ customer-centric/apps/ejecutivos-recuperacion/src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { CommonUiModule } from '@customer-centric/common-ui';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, CommonUiModule],
   providers: [],
   bootstrap: [AppComponent],
--- customer-centric/libs/services/src/lib/jarvis/jarvis.ts
+++ customer-centric/libs/services/src/lib/jarvis/jarvis.ts
@@ -7,12 +7,22 @@ export interface Cliente {
   export const exampleClientes: Cliente[] = [
     {
       id: '1',
-      name: 'Cliente 1',
+      name: 'Alvaro Alvarado 1',
       price: 100,
     },
     {
       id: '2',
-      name: 'Cliente 2',
+      name: 'Camilo Cardenas 2',
+      price: 200,
+    },
+    {
+      id: '3',
+      name: 'Ramiro Riuz 2',
+      price: 200,
+    },
+    {
+      id: '4',
+      name: 'Cristian Cristancho 2',
       price: 200,
     },
   ];
