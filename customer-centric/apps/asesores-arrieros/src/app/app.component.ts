5d33664b569b095fdfeb18aff2925b2f3a9c441d
Miguel Reyes <mareyes46@mimsena.edu.co>
2023-09-04 11:28:57 -0500

    proyectos con ui y services compartidos

--- customer-centric/apps/asesores-arrieros/src/app/app.component.ts
+++ customer-centric/apps/asesores-arrieros/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { exampleClientes } from '@customer-centric/jarvis';
 @Component({
   selector: 'customer-centric-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'asesores-arrieros';
+  clientes = exampleClientes;
 }
