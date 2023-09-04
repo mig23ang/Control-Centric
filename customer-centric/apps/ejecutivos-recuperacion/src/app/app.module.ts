293122e9ae548ea5e293b3cf83938677ce7e30be
Miguel Reyes <mareyes46@mimsena.edu.co>
2023-09-04 11:06:12 -0500

    agreganda navbar a todos los proyectos

--- customer-centric/apps/ejecutivos-recuperacion/src/app/app.module.ts
+++ customer-centric/apps/ejecutivos-recuperacion/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CommonUiModule } from '@customer-centric/common-ui';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './app.routes';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CommonUiModule],
+  imports: [
+    BrowserModule,
+    CommonUiModule,
+    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- customer-centric/libs/common-ui/src/index.ts
+++ customer-centric/libs/common-ui/src/index.ts
@@ -1,3 +1,5 @@
 export * from './lib/common-ui.module';
 
 export * from './lib/button/button.component';
+
+export * from './lib/navbar/navbar.component';
