bfff0c98c5bb2a9d1c87daa1cc072b1bd453b495
Miguel Reyes <mareyes46@mimsena.edu.co>
2023-09-04 09:42:31 -0500

    Agregando config global

--- customer-centric/apps/fic/src/app/app.module.ts
+++ customer-centric/apps/fic/src/app/app.module.ts
@@ -3,11 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { CommonUiModule } from '@customer-centric/common-ui';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
