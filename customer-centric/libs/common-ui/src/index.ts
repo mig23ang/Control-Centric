1296f85e7d04f8d31e6b85b430139dc9a14f8e89
Miguel Reyes <mareyes46@mimsena.edu.co>
2023-09-05 09:39:52 -0500

    carpeta navb

--- customer-centric/libs/common-ui/src/index.ts
+++ customer-centric/libs/common-ui/src/index.ts
@@ -2,4 +2,4 @@ export * from './lib/common-ui.module';
 
 export * from './lib/button/button.component';
 
-export * from './lib/navbar/navbar.component';
+export * from './lib/nav/navbar/navbar.component';
--- customer-centric/libs/common-ui/src/lib/common-ui.module.ts
+++ customer-centric/libs/common-ui/src/lib/common-ui.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './nav/navbar/navbar.component';
 import { PrimaryDirective } from './button/directives/primary.directive';
 
 @NgModule({
