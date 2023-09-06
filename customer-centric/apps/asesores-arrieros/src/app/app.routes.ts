005e239254feb2842ffc63445203f02db3c2d375
Miguel Reyes <mareyes46@mimsena.edu.co>
2023-09-06 11:24:16 -0500

    rutas y notfound ok

--- customer-centric/apps/asesores-arrieros/src/app/app.routes.ts
+++ customer-centric/apps/asesores-arrieros/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'fic',
+  },
+  {
+    path: 'fic',
+    loadChildren: () =>
+      import('./pages/pages.module').then((m) => m.PagesModule),
+  },
+  {
+    path: '**',
+    loadChildren: () =>{
+      return import('@customer-centric/not-found').then(
+        (m) => m.NotFoundModule
+      );
+    }
+  }
+];
