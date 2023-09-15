a743fff7bf44022e3b3391a71c325d968d628fb3
miguel angel reyes <miguel.reyesc@usantoto.edu.co>
2023-09-15 12:30:28 -0500

    eliminando storyblok

--- customer-centric/libs/common-ui/src/lib/common-ui.module.ts
+++ customer-centric/libs/common-ui/src/lib/common-ui.module.ts
@@ -2,10 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
 import { NavbarComponent } from './nav/navbar/navbar.component';
-import { PrimaryDirective } from './button/directives/primary.directive';
 import { FooterComponent } from './nav/footer/footer.component';
-import { NavbarDirective } from './nav/navbar/directives/navbar.directive';
-import { FooterDirective } from './nav/footer/directives/footer.directive';
 import { MenuLateralComponent } from './menu-lateral/menu-lateral.component';
 
 @NgModule({
@@ -13,10 +10,7 @@ import { MenuLateralComponent } from './menu-lateral/menu-lateral.component';
   declarations: [
     ButtonComponent,
     NavbarComponent,
-    PrimaryDirective,
     FooterComponent,
-    FooterDirective,
-    NavbarDirective,
     MenuLateralComponent,
   ],
   exports: [
--- customer-centric/libs/common-ui/src/lib/nav/footer/directives/footer.directive.ts
+++
@@ -1,9 +0,0 @@
-import { Directive } from '@angular/core';
-
-@Directive({
-  // eslint-disable-next-line @angular-eslint/directive-selector
-  selector: '[footer, footer-primary, footer-secondary]',
-})
-export class FooterDirective {
-  constructor() {}
-}
