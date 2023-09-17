a385158530fcf42d938abeb0af2ceb47645a225b
miguel angel reyes <miguel.reyesc@usantoto.edu.co>
2023-09-17 13:25:20 -0500

    cambios

---
+++ customer-centric/libs/common-ui/src/lib/components/chip/chip.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'customer-centric-chip',
+  templateUrl: './chip.component.html',
+  styleUrls: ['./chip.component.scss'],
+})
+export class ChipComponent {}
